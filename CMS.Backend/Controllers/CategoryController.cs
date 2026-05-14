635d03ba7441ce605db73a8edd156a2bc8717b71
haoquang <hao01679462492@gmail.com>
2026-05-14 10:44:41 +0700

    Fix embedded repository and add all frontend files

---
+++ CMS.Backend/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using CMS.Data.Entities; 
+
+namespace CMS.Backend.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list = new List<Category> {
+            new Category { Id = 1, Name = "Tin Công Nghệ", Description = "Review Laptop, AI" },
+            new Category { Id = 2, Name = "Giáo Dục", Description = "Thông tin tuyển sinh" }
+        };
+            return View(list); // Gửi danh sách này sang giao diện
+
+        }
+    }
+}
